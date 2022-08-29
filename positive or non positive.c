1b53774869f5fbc3806b94492b6d2aeae0b71a4a
Rohitgaurkar <111226408+Rohitgaurkar@users.noreply.github.com>
2022-08-30 02:09:12 +0530

    Create positive or non positive.c

---
+++ positive or non positive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("enter a number");
+    scanf("%d",&y);
+    if(y>0)
+    {
+        printf("positive");
+    }
+     else
+    {
+         printf("non positive");
+    }
+    return 0;
+}
